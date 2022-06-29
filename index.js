fd2443289bad696610bb595777df81a4653a6ac9
alexbotha <alexbotha@hotmail.co.uk>
2022-06-29 16:43:31 +0100

    commit tree 1503d2e2426c115ea1ceead81e538b2fbc86181f

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+
+if (distance <= 400){
+  return "This one is on me!"
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (distance >= 400 && distance <= 2000){
+return "That will be twenty bucks."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (distance > 2000 && distance < 2500){
+  return "I will gladly take your thirty bucks."
+}
+
+else if (distance >= 2500) {
+return "No can do."
+}
+}
+scuberGreetingForFeet(2001)
+
+///////////////////////////////////////////////////////////////////////////////////
+
+function ternaryCheckCity(city){
+return (city === "NYC" ? 'Ok, sounds good.' : 'No go.');
+}
+ternaryCheckCity()
+
+//////////////////////////////////////////////////////////////////////
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return tip = "Thank you so much."
+      break;
+    case 'not as generous':
+     return tip = 'Thank you.'
+      break;
+    default:
+      case 'Thanks for everything':
+      return tip = 'Bye.'
+      
+     
+}
 }
